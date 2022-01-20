d095f520b489a0b181d837c2f3acafd7b9620009
Elvie Rayl <erayl@student.21-school.ru>
2022-01-20 13:07:14 +0300

    last fix

--- philo/sleep_and_eat.c
+++ philo/sleep_and_eat.c
@@ -6,7 +6,7 @@
 /*   By: erayl <erayl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 22:44:03 by marvin            #+#    #+#             */
-/*   Updated: 2022/01/15 21:29:19 by erayl            ###   ########.fr       */
+/*   Updated: 2022/01/20 13:06:22 by erayl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ bool	i_die_in_progress(t_phil *self, t_ms time_of_work)
 
 	time_after_work = current_timestamp() + time_of_work;
 	expected_death_time = self->lifecfg->time_to_die + self->last_time_eated;
-	if (time_after_work > expected_death_time)
+	if (time_after_work >= expected_death_time)
 	{
 		die(self);
 		return (true);
